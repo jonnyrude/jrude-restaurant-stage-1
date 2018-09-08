89efe38f93a52ad6dbf67bd52f30a8ea6f0868d9
JR <jrkrelix_01@hotmail.com>
2018-09-08 09:48:13 -0600

    Changed link to buttons

--- js/main.js
+++ js/main.js
@@ -178,12 +178,14 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute('type', 'submit');
+  more.setAttribute('onclick', 'window.location.href=' + `'${more.href}'`);
+  li.append(more);
 
-  return li
+  return li;
 }
 
 /**
